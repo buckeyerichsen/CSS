dde8fad93bd2b52614d5a0c0e61874e06567ce52
buckeyerichsen <buckey.erichsen@gmail.com>
2019-09-25 16:26:49 -0400

    updated

--- src/main/java/com/example/demo/HomeController.java
+++ src/main/java/com/example/demo/HomeController.java
@@ -2,11 +2,17 @@ package com.example.demo;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class HomeController {
     @GetMapping("/")
     public String index(){
         return "Html";
+    }
+
+    @RequestMapping("/whatNotToDo")
+    public String profile(){
+        return "whatNotToDo";
     }
 }
